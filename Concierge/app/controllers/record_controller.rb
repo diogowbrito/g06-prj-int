c1be54c1a88caf4600eda9083b992e76c098d0f3
Peddro <p.gloria28@gmail.com>
2011-04-24 08:37:31 -0700

    Falta ver a string empty

--- Concierge/app/controllers/record_controller.rb
+++ Concierge/app/controllers/record_controller.rb
@@ -1,15 +1,45 @@
+require 'rexml/document'
+
 class RecordController < ApplicationController
 
   def record
 
-  @title = "Teste"
-  ttest = Element.new("text", "texto1")
-  ltest = Element.new("link", "texto2", "www.google.pt")
-  ttest2 = Element.new("text", "texto3")
-  ltest2 = Element.new("link", "texto4", "www.google.pt")
-  arraytest = [ttest2, ltest2]
-  listtest = EList.new("OMG Works!", arraytest)
-  @data = [ttest, ltest, listtest]
+#  @title = "Teste"
+#  ttest = Element.new("text", "texto1")
+#  ltest = Element.new("link", "texto2", "www.google.pt")
+#  ttest2 = Element.new("text", "texto3")
+#  ltest2 = Element.new("link", "texto4", "www.google.pt")
+#  arraytest = [ttest2, ltest2]
+#  listtest = EList.new("OMG Works!", arraytest)
+#  @data = [ttest, ltest, listtest]
+
+    file = File.new('/home/pedro/RubymineProjects/Concierge/app/views/tests/index.xml')
+    doc = REXML::Document.new file
+
+    @title = doc.root.attributes.get_attribute("title")
+    text_tags = []
+    @attributes = []
+
+    REXML::XPath.each(doc, "//text") {
+      |ele|
+      if ele.text.length == 1
+        REXML::XPath.each(doc, "//text/entity") {
+          |ent|
+           text_tags << Element.new("link", ent.text)
+        }
+      else
+        text_tags << Element.new("text", ele.text)
+        @attributes << ele.attributes.get_attribute("title").value
+      end
+
+    }
+
+#     REXML::XPath.each(doc, "//entity") {
+#      |ele|
+#      entities << Element.new("link", ele.text)
+#    }
+
+    @data = [text_tags]
 
     respond_to do |format|
       format.html
