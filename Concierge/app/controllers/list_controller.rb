d2ed13243479d4c7f139cb6bafad7e115903d539
Diogo Brito <diogowbrito@gmail.com>
2011-05-03 09:23:33 -0700

    ..

--- Concierge/app/controllers/list_controller.rb
+++ Concierge/app/controllers/list_controller.rb
@@ -1,25 +1,29 @@
 require 'rexml/document'
-
+    require 'open-uri'
 class ListController < ApplicationController
 
   def list
 
-    file = File.new('/home/pedro/list.xml')
-    doc = REXML::Document.new file
+ #   file = File.new('/home/pedro/list.xml')
+ #   doc = REXML::Document.new file
 
-    @title = doc.root.attributes.get_attribute("title")
-    list = []
+ #   @title = doc.root.attributes.get_attribute("title")
+ #   list = []
 
-    REXML::XPath.each(doc, "//item"){
-        |ele|
-        list << Element.new("link", ele.text, ele.attributes.get_attribute("href").value)
-      }
-      e_list = EList.new("List Test", list)
+ #   REXML::XPath.each(doc, "//item"){
+ #       |ele|
+ #       list << Element.new("link", ele.text, ele.attributes.get_attribute("href").value)
+ #     }
+ #     e_list = EList.new("List Test", list)
 
-      @data = [e_list]
+ #     @data = [e_list]
+
+
+      @contents = URI.parse('http://localhost:3001/people.xml').read
 
       respond_to do |format|
         format.html
+
       end
   end
 
