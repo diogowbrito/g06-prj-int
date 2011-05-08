facc5abd198970140db47a51372eaa1319cdea06
Diogo Brito <diogowbrito@gmail.com>
2011-05-08 08:50:20 -0700

    ..

---
+++ Concierge/app/controllers/people_list_request_controller.rb
@@ -0,0 +1,16 @@
+require 'open-uri'
+
+class PeopleListRequestController < ApplicationController
+
+
+  def peopleListRequest
+
+      @contents = URI.parse('http://localhost:3001/people.xml').read
+
+      respond_to do |format|
+        format.html
+        format.xml
+      end
+  end
+
+end
