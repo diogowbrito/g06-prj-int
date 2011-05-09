79cbe662d8758697a7ff69f45bb35e60ec1380f9
Peddro <p.gloria28@gmail.com>
2011-05-09 12:58:24 -0700

    ..

--- Concierge/app/controllers/record_controller.rb
+++ Concierge/app/controllers/record_controller.rb
@@ -1,11 +1,6 @@
-require 'rexml/document'
-require 'open-uri'
-
 class RecordController < ApplicationController
 
   def record
-#   /home/pedro/record.xml@contents = URI.parse('http://localhost:3001/people/1.xml').read
-
     respond_to do |format|
       format.html
       format.xml
