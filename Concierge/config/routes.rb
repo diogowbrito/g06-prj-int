986ede33b9246b13fd6d10c45ee5d5a2ce850874
Diogo Brito <diogowbrito@gmail.com>
2011-05-09 09:34:33 -0700

    ..

--- Concierge/config/routes.rb
+++ Concierge/config/routes.rb
@@ -12,7 +12,8 @@ Concierge::Application.routes.draw do
 
   match "record" => "Record#record"
   match "list" => "List#list"
-  match "peopleListRequest" => "PeopleListRequest#peoplelistrequest"
+  match "peoplelistrequest" => "PeopleListRequest#peoplelistrequest", :defaults => {:format  => :xml }
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
