81e3b0a64ed33934b16ca27a779c9ed80ce41330
Diogo Brito <diogowbrito@gmail.com>
2011-05-09 11:13:31 -0700

    ..

--- Concierge/config/application.rb
+++ Concierge/config/application.rb
@@ -33,6 +33,10 @@ module Concierge
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
+
+    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.6 jqtouch)
+
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
