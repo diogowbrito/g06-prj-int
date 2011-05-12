12371f5e6333487acb09d9345427cf7048cce97b
Joao Horta <jphorta.volt@gmail.com>
2011-05-12 15:53:41 +0100

    ..

--- Concierge/app/controllers/search_controller.rb
+++ Concierge/app/controllers/search_controller.rb
@@ -3,31 +3,40 @@ class SearchController < ApplicationController
 
    def search
 
-    @keyword =  params[:keyword].gsub("%", "\%").gsub("_", "\_")
+    @keyword =  params[:keyword].gsub("%", "\%").gsub("_", "\_").gsub(" ", "+")
     @start = (params[:start] || '1').to_i
     @end = (params[:end] || '20').to_i
     counter = 1
     services = Service.order(:ranking)
     list = []
     flag = 0
-
     services.each do |service|
 
+      puts "entrou"
       competence = service.competences.where(:competenceType => "Search")
       url = competence[0].competenceUrl
-      list << Nokogiri::XML(open(url+'?keyword='+@keyword+"&start=1&end=5000"),nil, 'UTF-8')
+      homeurl = service.url
+      tempdoc = Nokogiri::XML(open(url+'?keyword='+@keyword+"&start=1&end=5000"),nil, 'UTF-8')
+      temproot = tempdoc.at_css "list"
+      temproot.add_child("<home>"+homeurl+"</home>")
+      list << tempdoc
 
     end
     @doc = Nokogiri::XML("<list></list>")
 
     list.each do |result|
 
+      homenodes = result.xpath("//home")
+      homeurl = homenodes[0].content
       nodes = result.xpath("//item")
 
       nodes.each do |node|
 
         if counter >= @start then
         root = @doc.at_css "list"
+        href = node['href']
+        link = href.gsub(homeurl, "http://localhost:3000/services")
+        node['href'] = link
         root.add_child(node)
         end
         counter = counter + 1
@@ -39,6 +48,7 @@ class SearchController < ApplicationController
       if flag == 0 then
         break
       end
+
     end
 
     respond_to :xml
@@ -47,15 +57,26 @@ class SearchController < ApplicationController
 
   def servicesearch
 
-    @keyword =  params[:keyword].gsub("%", "\%").gsub("_", "\_")
+    @keyword =  params[:keyword].gsub("%", "\%").gsub("_", "\_").gsub(" ", "+")
     @start = (params[:start] || '1')
     @end = (params[:end] || '20')
-    @service = Service.where(:serviceName => params[:service])
-    @competence = @service[0].competences.where(:competenceType => "Search")
-    @url = @competence[0].competenceUrl
-    @doc = Nokogiri::XML(open(@url+'?keyword='+@keyword+"&start="+@start+"&end="+@end),nil, 'UTF-8')
-    respond_to :xml
+    @servicename = params[:service]
+    service = Service.where(:serviceName => @servicename)
+    competence = service[0].competences.where(:competenceType => "Search")
+    homeurl = service[0].url
 
+    url = competence[0].competenceUrl
+    @doc = Nokogiri::XML(open(url+'?keyword='+@keyword+"&start="+@start+"&end="+@end),nil, 'UTF-8')
+
+    nodes = @doc.xpath("//item")
+
+    nodes.each do |node|
+      href = node['href']
+      link = href.gsub(homeurl, "http://localhost:3000/services")
+      node['href'] = link
+    end
+
+    respond_to :xml
   end
 
 end
--- Concierge/config/routes.rb
+++ Concierge/config/routes.rb
@@ -3,13 +3,13 @@ Concierge::Application.routes.draw do
   root :to => "HomePage#index"
   match "index" => "HomePage#index"
   match "services/:service/index" => "ServiceForward#homepagerequest", :defaults => { :format => :xml}
+  match "search" => "Search#search", :defaults => { :format => :xml}
+  match "services/:service/search" => "Search#servicesearch", :defaults => { :format => :xml}
 #  match "services/:service/:list" => "List#list"
   match "services/:service/:id" => "Record#record"
   match "recordAux" => "Record#recordAux", :defaults => { :format => :xml}
   match "list" => "List#list"
   match "peoplelistrequest" => "PeopleListRequest#peoplelistrequest", :defaults => { :format => :xml}
-  match "search" => "Search#search", :defaults => { :format => :xml}
-  match "services/:service/search" => "Search#servicesearch", :defaults => { :format => :xml}
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
