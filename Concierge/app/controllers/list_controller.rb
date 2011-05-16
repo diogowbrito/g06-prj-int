c03beedee71487f3ebd2c5bf5c8d7e6523dd9770
Diogo Brito <diogowbrito@gmail.com>
2011-05-16 15:36:21 -0700

    ..

--- Concierge/app/controllers/list_controller.rb
+++ Concierge/app/controllers/list_controller.rb
@@ -3,10 +3,14 @@ class ListController < ApplicationController
 
     def list
       @service = params[:service]
-      @id = params[:id]
-      @link = "http://localhost:3001/"+@service+".xml/"
-      @doc = Nokogiri::XML(open(@link),nil, 'UTF-8')
+      @method = params[:method]
+      service = Service.where(:serviceName => @servicename)
+
+      serviceurl = service[0].url
+      link = "serviceurl" + method
+      @doc = Nokogiri::XML(open(link),nil, 'UTF-8')
       respond_to :xml
   end
+
 
 end
