61bf26cc8442e2748d94198d15941d0301772d4b
Pedro Gloria <p.gloria28@gmail.com>
2011-05-17 11:53:59 +0100

    ..

--- Concierge/public/javascripts/application.js
+++ Concierge/public/javascripts/application.js
@@ -2,7 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 function getHomepage(url) {
-
     $(document).ready(function() {
         $.ajax({
             type: "GET",
@@ -14,6 +13,20 @@ function getHomepage(url) {
     });
 
 }
+
+$('#resource').load(function() {
+    alert("oi");
+    $(document).ready(function() {
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:3000/record?service=people&id=1",
+            dataType: "xml",
+            success: parseXml
+        });
+        $('.hidden').remove();
+    });
+});
+
 function getSearch() {
 
 $("#content").append("<h1>Search</h1>");
@@ -62,6 +75,66 @@ function parseHomepage(xml) {
 
     });
 }
+
+ function parseXml(xml) {
+        $(xml).find("record").children().each(function(index, element) {
+            parseRecord($(this), element);
+        });
+    }
+
+    function parseRecord(node, element) {
+        var text;
+        var title;
+        switch (element.nodeName) {
+            case 'text':
+                if (node.children().size() == 0) {
+                    text = node.text();
+                    title = node.attr('title');
+                    if (title == undefined)
+                        $("#resource").append("<p><a href=" + attr + ">" + text + "</a></p>");
+                    else
+                        $("#resource").append("<p>" + title + ": " + text + "</p>");
+                }
+                else {
+                    title = node.attr('title');
+                    if (title != undefined)
+                        $("#resource").append("<p>" + title + "</p>");
+
+                    node.children().each(function(index, element) {
+                        if (element.nodeName == 'entity') {
+                            text = $(this).text();
+                            var attr = $(this).attr('href');
+                            $("#resource").append("<p><a href=" + attr + ">" + text + "</a></p>");
+                        }
+                        else if (element.nodeName == 'text') {
+                            text = $(this).text();
+                            if (title == undefined)
+                                $("#resource").append("<p>" + text + "</p>");
+                        }
+                    });
+                }
+                break;
+
+            case 'entity':
+                text = node.text();
+                title = node.attr('title');
+                var attr = node.attr('href');
+                if (title == undefined)
+                    $("#resource").append("<p><a href=" + attr + ">" + text + "</a></p>");
+                else
+                    $("#resource").append("<p>" + title + ": " + "<a href=" + attr + ">" + text + "</a></p>");
+                break;
+
+            case 'email':
+                text = node.text();
+                title = node.attr('title');
+                if (title == undefined)
+                    $("#resource").append("<p><a href=" + attr + ">" + text + "</a></p>");
+                else
+                    $("#resource").append("<p>" + title + ": " + text + "</p>");
+                break;
+        }
+    }
 
 $('#serviceLink').live('click', function(){
       getHomepage($(this).attr('href'));
